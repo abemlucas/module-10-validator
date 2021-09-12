cb21461e6d684f7859c8cf310c6718c6111af3aa
Abem Lucas <abemlucas12@gmail.com>
2021-09-12 16:40:54 -0700

    Updated luhn2.py file

---
+++ luhn2.py
@@ -0,0 +1,45 @@
+# Input to store the credit card number.
+creditcard = int(input("Credit card number: "))
+
+# Reversing the creditcard number
+Reverse = 0
+while(creditcard > 0):
+    Reminder = creditcard % 10
+    Reverse = (Reverse * 10) + Reminder
+    creditcard = creditcard // 10
+
+creditcard_str = str(Reverse)
+sum1 = 0
+sum2 = 0
+
+# To check for the last digit of the total.
+
+
+def last_digit(num):
+    num = str(num)
+    return int(num[len(num)-1])
+
+
+# For the construction of s1.
+for i, element in enumerate(creditcard_str):
+    if i % 2 == 0:
+        sum1 += int(element)
+
+# For the even digit placement.
+for i, element in enumerate(creditcard_str):
+    if i % 2 != 0:
+        partial_sum = int(element)*2
+
+        if partial_sum > 9:
+            new_sum = partial_sum-9
+            sum2 += int(new_sum)
+        else:
+            sum2 += int(partial_sum)
+
+total = sum1+sum2
+
+# Checks if card is valid or not.
+if (last_digit(total) == 0):
+    print("VALID CARD")
+else:
+    print("INVALID CARD")
