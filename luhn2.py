e8b72f9b0474742a274d0198a85fb3ba987df41f
Abem Lucas <abemlucas12@gmail.com>
2021-09-12 16:58:48 -0700

    Updated luhn2.py

--- luhn2.py
+++ luhn2.py
@@ -1,45 +1,50 @@
-# Input to store the credit card number.
-creditcard = int(input("Credit card number: "))
+import csv
 
-# Reversing the creditcard number
-Reverse = 0
-while(creditcard > 0):
-    Reminder = creditcard % 10
-    Reverse = (Reverse * 10) + Reminder
-    creditcard = creditcard // 10
+with open('numbers.csv') as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
 
-creditcard_str = str(Reverse)
-sum1 = 0
-sum2 = 0
+    for row in csvReader:
+        print("\n", row[0])
 
-# To check for the last digit of the total.
+        creditcard = row[0]
 
+        # Reversing the creditcard number
+        Reverse = 0
+        while(int(creditcard) > 0):
+            Reminder = int(creditcard) % 10
+            Reverse = (Reverse * 10) + Reminder
+            creditcard = int(creditcard) // 10
 
-def last_digit(num):
-    num = str(num)
-    return int(num[len(num)-1])
+        creditcard_str = str(Reverse)
+        sum1 = 0
+        sum2 = 0
 
+        # To check for the last digit of the total.
 
-# For the construction of s1.
-for i, element in enumerate(creditcard_str):
-    if i % 2 == 0:
-        sum1 += int(element)
+        def last_digit(num):
+            num = str(num)
+            return int(num[len(num)-1])
 
-# For the even digit placement.
-for i, element in enumerate(creditcard_str):
-    if i % 2 != 0:
-        partial_sum = int(element)*2
+        # For the construction of s1.
+        for i, element in enumerate(creditcard_str):
+            if i % 2 == 0:
+                sum1 += int(element)
 
-        if partial_sum > 9:
-            new_sum = partial_sum-9
-            sum2 += int(new_sum)
+        # For the even digit placement.
+        for i, element in enumerate(creditcard_str):
+            if i % 2 != 0:
+                partial_sum = int(element)*2
+
+                if partial_sum > 9:
+                    new_sum = partial_sum-9
+                    sum2 += int(new_sum)
+                else:
+                    sum2 += int(partial_sum)
+
+        total = sum1+sum2
+
+        # Checks if card is valid or not.
+        if (last_digit(total) == 0):
+            print("VALID CARD")
         else:
-            sum2 += int(partial_sum)
-
-total = sum1+sum2
-
-# Checks if card is valid or not.
-if (last_digit(total) == 0):
-    print("VALID CARD")
-else:
-    print("INVALID CARD")
+            print("INVALID CARD")
