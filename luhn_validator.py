ab087b66a66b18d7e70bf0fc467860a4c992fa87
Abem Lucas <abemlucas12@gmail.com>
2021-09-13 19:09:37 -0700

    Shortened code for the luhn_validator

--- luhn_validator.py
+++ luhn_validator.py
@@ -1,7 +1,4 @@
-# Input to store the credit card number.
 creditcard = int(input("Credit card number: "))
-
-# Reversing the creditcard number
 Reverse = 0
 while(creditcard > 0):
     Reminder = creditcard % 10
@@ -9,10 +6,7 @@ while(creditcard > 0):
     creditcard = creditcard // 10
 
 creditcard_str = str(Reverse)
-sum1 = 0
-sum2 = 0
-
-# To check for the last digit of the total.
+sum1 = 0, sum2 = 0
 
 
 def last_digit(num):
@@ -20,14 +14,10 @@ def last_digit(num):
     return int(num[len(num)-1])
 
 
-# For the construction of s1.
 for i, element in enumerate(creditcard_str):
     if i % 2 == 0:
         sum1 += int(element)
-
-# For the even digit placement.
-for i, element in enumerate(creditcard_str):
-    if i % 2 != 0:
+    elif i % 2 != 0:
         partial_sum = int(element)*2
 
         if partial_sum > 9:
@@ -38,7 +28,6 @@ for i, element in enumerate(creditcard_str):
 
 total = sum1+sum2
 
-# Checks if card is valid or not.
 if (last_digit(total) == 0):
     print("VALID CARD")
 else:
