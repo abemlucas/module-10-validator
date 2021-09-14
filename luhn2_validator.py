788c985b1b0907c16f448795a4a23456d1f8a302
Abem Lucas <abemlucas12@gmail.com>
2021-09-13 19:11:01 -0700

    Shortened the code for luhn2_validator

--- luhn2_validator.py
+++ luhn2_validator.py
@@ -5,10 +5,8 @@ with open('numbers.csv') as csvDataFile:
 
     for row in csvReader:
         print("\n", row[0])
-
         creditcard = row[0]
 
-        # Reversing the creditcard number
         Reverse = 0
         while(int(creditcard) > 0):
             Reminder = int(creditcard) % 10
@@ -16,23 +14,16 @@ with open('numbers.csv') as csvDataFile:
             creditcard = int(creditcard) // 10
 
         creditcard_str = str(Reverse)
-        sum1 = 0
-        sum2 = 0
-
-        # To check for the last digit of the total.
+        sum1 = 0, sum2 = 0
 
         def last_digit(num):
             num = str(num)
             return int(num[len(num)-1])
 
-        # For the construction of s1.
         for i, element in enumerate(creditcard_str):
             if i % 2 == 0:
                 sum1 += int(element)
-
-        # For the even digit placement.
-        for i, element in enumerate(creditcard_str):
-            if i % 2 != 0:
+            elif i % 2 != 0:
                 partial_sum = int(element)*2
 
                 if partial_sum > 9:
@@ -40,10 +31,7 @@ with open('numbers.csv') as csvDataFile:
                     sum2 += int(new_sum)
                 else:
                     sum2 += int(partial_sum)
-
         total = sum1+sum2
-
-        # Checks if card is valid or not.
         if (last_digit(total) == 0):
             print("VALID CARD")
         else:
